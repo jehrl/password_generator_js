8c804fae71b158cce5d4657860e9f1aee7f93481
Jan Ehrlich <126582876+jehrl@users.noreply.github.com>
2023-10-03 16:09:49 +0200

    repare of classes

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 import generator from "./modules/generator.js"
 
 //Classses
-class Includes {
+class arrIncludes {
     constructor(arr) {
-        this.arr = arr
+        this._arr = arr
     }
     get arr(){
         return this.arr
     }
 }
 
-const upperCaseAlphabet = new Includes(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']);
-const lowerCaseAlphabet = new Includes(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']);
-const numbers = new Includes ([1,2,3,4,5,6,7,8,9,10]);
-const specialCharacters = new Includes(['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '[', ']', '{', '}', '|', ';', ':', "'", '"', ',', '.', '/', '<', '>', '?']);
+const upperCaseAlphabet = new arrIncludes(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']);
+const lowerCaseAlphabet = new arrIncludes(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']);
+const numbers = new arrIncludes ([1,2,3,4,5,6,7,8,9,10]);
+const specialCharacters = new arrIncludes(['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '[', ']', '{', '}', '|', ';', ':', "'", '"', ',', '.', '/', '<', '>', '?']);
 
 const upperCaseAlphabetCheckbox = document.getElementById("upperCase")
 const lowerCaseAlphabetCheckbox = document.getElementById("lowerCase")
